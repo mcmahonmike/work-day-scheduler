e21bcfb749eab415faa24fcc164c198d47c0aa39
Hephaestus <mcmahonmike9@yahoo.com>
2022-04-09 13:27:18 -0500

    re commit timeblock function

--- assets/custom.js
+++ assets/custom.js
@@ -1,10 +1,18 @@
 
 var currentDate = moment().format("MMMM Do, YYYY - hh:mm:ss a");
-var currentTime = moment().format('H')
 var hour = $(".description")
 
+
+
+
+
 var Time = function(){
-$("#currentDay").text(currentDate)
+    var currentTime = moment().hour()
+$(".time-block").each(function(){
+    var timeChecker = ($(this).attr("id").split("hour")[1]);
+    var onHour = $(this).attr("id");
+    $("#" + onHour + ".description").val(localStorage.getItem(onHour));
+})
 }
 
 Time()
