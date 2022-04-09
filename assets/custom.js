9879ac0860b5c55fe691accdb2b77d9a4358af01
Hephaestus <mcmahonmike9@yahoo.com>
2022-04-09 13:54:00 -0500

    time function operating

--- assets/custom.js
+++ assets/custom.js
@@ -7,17 +7,27 @@ var hour = $(".description")
 
 
 var Time = function(){
+    // creating variable for the current hour
     var currentTime = moment().hour()
-$(".time-block").each(function(){
+    // looping through each time block and accessing ids
+    $(".time-block").each(function(){
     var timeChecker = ($(this).attr("id").split("hour")[1]);
     var onHour = $(this).attr("id");
     $("#" + onHour + ".description").val(localStorage.getItem(onHour));
+    // if statement to add class styles to timeblocks
+    if (currentTime < timeChecker){
+        $(this).addClass("future")
+    }else if (currentTime == timeChecker) {
+        $(this).addClass("present")
+    }else{
+        $(this).addClass("past")
+    }
 })
 }
 
 Time()
 
-$(hour).addClass('past')
+
 
  
 
